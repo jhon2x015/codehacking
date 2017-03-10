d8c0be5d08ce8d22b55525116a418eccd5499785
Vryle <jv.paulate@gmail.com>
2017-03-10 13:44:38 +0800

    admin master page setup

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,30 @@ var elixir = require('laravel-elixir');
  |
  */
 
-elixir(function(mix) {
-    mix.sass('app.scss');
+elixir(function(mix) 
+{
+
+    mix.sass('app.scss')
+
+    .styles([
+
+    	'libs/blog-post.css',
+    	'libs/bootstrap.css',
+    	'libs/font-awesome.css',
+    	'libs/metisMenu.css',
+    	'libs/sb-admin-2.css',
+    	'libs/styles.css',
+    	'libs/timeline.css'
+
+    ], './public/css/libs.css')
+
+    .scripts([
+    	'libs/jquery.js',
+    	'libs/bootstrap.min.js',
+    	'libs/metisMenu.js',
+    	'libs/sb-admin-2.js'
+
+    ], './public/js/libs.js');
+
+
 });
